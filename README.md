669c530c6262198c905fceecf93969ae42939cbc
Simon Szalai <simon.szalai@gmail.com>
2022-11-16 19:20:29 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Welcome!
+
+This repo is the starting point of the **Practical Data Engineering with Dagster & Deepnote** course on Udemy.
+
+To get started, simply click the button below:
+[<img src="https://deepnote.com/buttons/launch-in-deepnote.svg">](https://deepnote.com/launch?url=https%3A%2F%2Fgithub.com%2Fnumentum%2Fpractical-data-engineering)
---
+++ constants.py
@@ -0,0 +1,25 @@
+PAYMENT_METHODS = ["cash", "credit", "debit"]
+
+LOCATIONS = ["main_farm", "supermarket", "auntie_debbies_shop", "full_moon_market"]
+
+EMPLOYEES = ["james", "sarah", "carmen", "peter"]
+
+PRODUCTS = {
+    24625356: "strawberries",
+    98320088: "blueberries",
+    83846512: "blackberries",
+    98623454: "blackcurrants",
+    87245676: "salmonberries",
+    12635273: "raspberries"
+}
+
+UNIT_PRICES = {
+    24625356: 6.99,
+    98320088: 8.99,
+    83846512: 4.99,
+    98623454: 3.49,
+    87245676: 10.99,
+    12635273: 10.49
+}
+
+TAX_RATE = 0.05
